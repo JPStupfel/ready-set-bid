d94b3f555b2d0c414631584adc1b90afc5aa4f28
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-08-23 09:49:53 -0500

    maps is working

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import MapContainer from './MapContainer';
 
 function App() {
   return (
     <div className="App">
+      <MapContainer />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
