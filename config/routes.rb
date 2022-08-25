c5ceafbb044fbc6010294b8d47ee846e8cccb587
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-08-25 16:38:35 -0500

    moved react router dom dependency to package.json file in client

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   get "/latest", to: 'posts#latest'
   get '/api', to: "api#index"
+  
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
 
