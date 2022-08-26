01d0d05f52590e3a18ac421431a74ded6294bdea
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-08-26 12:10:30 -0500

    has button that turns blue to select client or pro at signup

--- client/src/components/SignupContainer.js
+++ client/src/components/SignupContainer.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
 import SignupClientForm from './SignupClientForm'
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ToggleButton from 'react-bootstrap/ToggleButton';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export default function SignupContainer() {
+
+    const [checked, setChecked] = useState(false);
+    const [radioValue, setRadioValue] = useState('1');
+
+    const radios = [
+        { name: 'I am a Client', value: '1' },
+        { name: 'I am a Professional', value: '2' }      ];
+    
+
   return (
+    <>
     <SignupClientForm />
+    <ButtonGroup className="mb-2">
+        {radios.map((radio, idx) => (
+          <ToggleButton
+            key={idx}
+            id={`radio-${idx}`}
+            type="radio"
+            variant="secondary"
+            name="radio"
+            value={radio.value}
+            checked={radioValue === radio.value}
+            style={radioValue === radio.value ? {'backgroundColor':'blue'} : null}
+            onChange={(e) => setRadioValue(e.currentTarget.value)}
+          >
+            {radio.name}
+          </ToggleButton>
+        ))}
+      </ButtonGroup>
+    </>
   )
 }
