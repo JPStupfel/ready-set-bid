71134ba7dda1c9eedb572dcf8a71d34e67416d08
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-08-27 21:30:27 -0500

    has project card shown on project page

--- client/src/components/ProjectsPage.js
+++ client/src/components/ProjectsPage.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import MapContainer from './MapContainer'
+import ProjectCard from './ProjectCard';
 
 export default function ProjectsPage() {
+
+
+
   return (
     <div>
         <MapContainer/>
+        <ProjectCard />
     </div>
   )
 }
