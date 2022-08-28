92261199572c624742d7c4117852c925bfba1737
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-08-27 21:40:47 -0500

    proposal index route has before action require login

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -1,7 +1,16 @@
 class ProposalsController < ApplicationController
-
+    before_action :require_login
+    
     def index
         proposals = Proposal.all
         render json: proposals, status: 200
     end
+
+    private
+    def require_login
+        if !session[:user_id] 
+            render json: {error: 'log in first'}
+        end
+    end
+
 end
