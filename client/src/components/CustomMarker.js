9288a06b2bab3b4e10e8dfe0adf0454193cacf3c
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-08-30 08:07:21 -0500

    pop up contains image

--- client/src/components/CustomMarker.js
+++ client/src/components/CustomMarker.js
@@ -8,18 +8,21 @@ export default function CustomMarker({project}) {
 
         const location = {lat:project.lat, lng: project.lng}
 
+        const image = project.posts.length ? project.posts[0].image_url : ''
+
+
         const info =    
         <InfoBox
         position={location}
         options={{  closeBoxURL: ``, enableEventPropagation: true }}
         onCloseClick={()=>console.log('deed')}
-      
-
-      >
-        <div style={{ backgroundColor: `yellow` }}>
+        >
+        <div style={{ backgroundColor: `yellow`,' max-width':'200px' }}>
           <div style={{ fontSize: `16px`, fontColor: `#08233B` }}>
             {project.title}
           </div>
+          <img src={image} className='map-image' />
+          
         </div>
       </InfoBox>
 
