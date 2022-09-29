0ebd8126894d75423a9f616a276265c06aab6233
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-09-28 19:58:02 -0500

    changed google_api_key to REACT_APP_API_KEY_GOOGLE_MAPS_JS because API key must begin with REACT_APP

---
+++ client/src/ProjectClientCard.js
@@ -0,0 +1,26 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { useNavigate } from 'react-router-dom';
+
+
+
+function ProjectClientCard({project}) {
+  const history = useNavigate();
+  // if (project.title == 'q'){debugger}
+  const image = project.posts.length ? project.posts[0].image_url : 'fish'
+
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={image} />
+      <Card.Body>
+        <Card.Title>{project.title}</Card.Title>
+        <Card.Text>
+          {project.description}
+        </Card.Text>
+        <Button variant="primary" onClick={()=>history(`/myprojects/${project.id}`)}>View Project</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default ProjectClientCard;
---
+++ client/src/ProjectProfessionalCard.js
@@ -0,0 +1,26 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { useNavigate } from 'react-router-dom';
+
+
+
+function ProjectProfessionalCard({project}) {
+  const history = useNavigate();
+
+  const image = project.posts.length ? project.posts[0].image_url : ''
+
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={image} />
+      <Card.Body>
+        <Card.Title>{project.title}</Card.Title>
+        <Card.Text>
+          {project.description}
+        </Card.Text>
+        <Button variant="primary" onClick={()=>history(`/projects/${project.id}`)}>View Project</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default ProjectProfessionalCard;
