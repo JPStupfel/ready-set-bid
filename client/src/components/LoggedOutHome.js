599bdf5026bea6e8183f2e4c7be75b8cd9acdc6a
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-10-04 16:28:10 -0500

    added faded in button for guest user

--- client/src/components/LoggedOutHome.js
+++ client/src/components/LoggedOutHome.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Container from 'react-bootstrap/esm/Container'
+import Button from 'react-bootstrap/Button';
 
 export default function LoggedOutHome() {
+
+    const [fadeProp, setFadeProp] = useState(false)
+
+    useEffect(()=>{
+        const timeOut = setTimeout(()=>{setFadeProp(true);}, 700)
+
+        //  return () => clearInterval(timeOut)
+    },[])
+
   return (
-    <div>Welcome to Ready Set Bid! Please Login ;-)</div>
-  )
+        <Container>
+            <h1>Welcome to Ready Set Bid!</h1>
+            <h2>Log in or Signup!</h2>
+            <fader className={fadeProp ? 'fade-in' : 'fade-out'}>  
+            
+                    <div className="d-grid gap-2">
+                            <Button  variant="primary" size="lg">
+                                Or Explore the site as a guest
+                            </Button>      
+                    </div> 
+                
+            </fader>
+        </Container>
+    
+
+    )
 }
