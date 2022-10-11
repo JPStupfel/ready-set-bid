700d29f8478ad63007e90ce7e240bc2232abc7e1
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-10-11 17:05:44 -0500

    can now ref templates scss file

--- client/src/components/LoginClientForm.js
+++ client/src/components/LoginClientForm.js
@@ -53,3 +53,24 @@ function handleChange(event){
   </form>
   )
 }
+
+
+              // <form onSubmit={this.handleSubmit}>
+							// 	<div className="form-floating mb-20px">
+							// 		<input type="email" className="form-control fs-13px h-45px" id="emailAddress" placeholder="Email Address" />
+							// 		<label htmlFor="emailAddress" className="d-flex align-items-center py-0">Email Address</label>
+							// 	</div>
+							// 	<div className="form-floating mb-20px">
+							// 		<input type="password" className="form-control fs-13px h-45px" id="password" placeholder="Password" />
+							// 		<label htmlFor="password" className="d-flex align-items-center py-0">Password</label>
+							// 	</div>
+							// 	<div className="form-check mb-20px">
+							// 		<input className="form-check-input" type="checkbox" value="" id="rememberMe" />
+							// 		<label className="form-check-label" htmlFor="rememberMe">
+							// 			Remember Me
+							// 		</label>
+							// 	</div>
+							// 	<div className="login-buttons">
+							// 		<button type="submit" className="btn h-45px btn-success d-block w-100 btn-lg">Sign me in</button>
+							// 	</div>
+							// </form>
--- client/src/components/LoginContainer.js
+++ client/src/components/LoginContainer.js
@@ -3,6 +3,7 @@ import LoginClientForm from './LoginClientForm'
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import ToggleButton from 'react-bootstrap/ToggleButton';
 import LoginProForm from './LoginProForm';
+import '/Users/jpstupfel/Development/code/phase-5/ready-set-bid/client/src/components/scss/react.scss'
 
 
 export default function LoginContainer({setUser}) {
@@ -33,10 +34,50 @@ export default function LoginContainer({setUser}) {
           </ToggleButton>
         ))}
       </ButtonGroup>
-
+{/* 
       <div className='signupContainer'>
       {radioValue === '1' ?   <LoginClientForm setUser={setUser} /> : <LoginProForm setUser={setUser}/>}
-      </div>
+      </div> */}
+
+<div className="login login-v1">
+				<div className="login-container">
+					<div className="login-header">
+						<div className="brand">
+							<div className="d-flex align-items-center">
+								<span className="logo"></span> <b>Color</b> Admin
+							</div>
+							<small>Bootstrap 5 Responsive Admin Template</small>
+						</div>
+						<div className="icon">
+							<i className="fa fa-lock"></i>
+						</div>
+					</div>
+					<div className="login-body">
+						<div className="login-content fs-13px">
+							<form onSubmit={console.log('hi')}>
+								<div className="form-floating mb-20px">
+									<input type="email" className="form-control fs-13px h-45px" id="emailAddress" placeholder="Email Address" />
+									<label htmlFor="emailAddress" className="d-flex align-items-center py-0">Email Address</label>
+								</div>
+								<div className="form-floating mb-20px">
+									<input type="password" className="form-control fs-13px h-45px" id="password" placeholder="Password" />
+									<label htmlFor="password" className="d-flex align-items-center py-0">Password</label>
+								</div>
+								<div className="form-check mb-20px">
+									<input className="form-check-input" type="checkbox" value="" id="rememberMe" />
+									<label className="form-check-label" htmlFor="rememberMe">
+										Remember Me
+									</label>
+								</div>
+								<div className="login-buttons">
+									<button type="submit" className="btn h-45px btn-success d-block w-100 btn-lg">Sign me in</button>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+
     </>
   
   )
