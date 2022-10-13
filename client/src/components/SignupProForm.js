cb4c4a681652e1a489ac945fea71456857343e5c
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-10-13 11:03:38 -0500

    signup pro form has style

--- client/src/components/SignupProForm.js
+++ client/src/components/SignupProForm.js
@@ -24,38 +24,41 @@ function handleChange(event){
     newFormData[id] = input
     setFormData(newFormData)
 }
-  return (
-    <div>
-        <h1>Sign up as a Professional</h1>
-    <form onSubmit={handleSubmit}>
-
-    <div className="form-group">
-      <label>Type a Username</label>
-      <input onChange={handleChange} type="text" className="form-control" id="username"  placeholder="Enter username"/>
+return (
+  <div className="login-body">
+    <div className="login-content fs-13px">
+      <form onSubmit={handleSubmit}>
+        <div className="form-floating mb-20px">
+          <input onChange={handleChange}   type="text" className="form-control fs-13px h-45px" id="username" placeholder="Username" />
+          <label htmlFor="username" className="d-flex align-items-center py-0">Username</label>
+        </div>
+        <div className="form-floating mb-20px">
+          <input onChange={handleChange}  type="password" className="form-control fs-13px h-45px" id="password" placeholder="Password" />
+          <label htmlFor="password" className="d-flex align-items-center py-0">Password</label>
+        </div>
+        <div className="form-floating mb-20px">
+          <input onChange={handleChange}  type="password" className="form-control fs-13px h-45px" id="password_confirmation" placeholder="Confirm Password" />
+          <label htmlFor="password_confirmation" className="d-flex align-items-center py-0">Confirm Password</label>
+        </div>
+        <div className="form-floating mb-20px">
+          <input onChange={handleChange}  type="text" className="form-control fs-13px h-45px" id="image_url" placeholder="Profile Picture Url" />
+          <label htmlFor="password_confirmation" className="d-flex align-items-center py-0">Profile Picture Url</label>
+        </div>
+        <div className="form-floating mb-20px">
+          <input onChange={handleChange}  type="text" className="form-control fs-13px h-45px" id="email" placeholder="Enter Email Address" />
+          <label htmlFor="password_confirmation" className="d-flex align-items-center py-0">Enter Email Address</label>
+        </div>
+        <div className="form-check mb-20px">
+          <input className="form-check-input" type="checkbox" value="" id="rememberMe" />
+          <label className="form-check-label" htmlFor="rememberMe">
+            Remember Me
+          </label>
+        </div>
+        <div className="login-buttons">
+          <button type="submit" className="btn h-45px btn-success d-block w-100 btn-lg">Sign me up</button>
+        </div>
+      </form>
     </div>
-
-    <div className="form-group">
-      <label>Password</label>
-      <input onChange={handleChange}  type="password" className="form-control" id="password" placeholder="Password"/>
-    </div>
-
-  
-
-    <div className="form-group">
-      <label>Password Confirmation</label>
-      <input onChange={handleChange}  type="password" className="form-control" id="password_confirmation" placeholder="Confirm Password"/>
-    </div>
-
-    <div className="form-group">
-      <label>Profile Picture Url</label>
-      <input onChange={handleChange}  type="text" className="form-control" id="image_url" placeholder="Profile Picture Url"/>
-    </div>
-
-    <div className="form-check">
-    </div>
-    
-    <button type="submit" className="btn btn-primary">Submit</button>
-  </form>
   </div>
-  )
+)
 }
