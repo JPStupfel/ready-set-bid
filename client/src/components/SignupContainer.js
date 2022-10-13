1cc2ed6604532207545db30f6579e05d83b488e6
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-10-13 10:48:57 -0500

    signup container has style

--- client/src/components/SignupContainer.js
+++ client/src/components/SignupContainer.js
@@ -15,28 +15,40 @@ export default function SignupContainer({setUser}) {
     
 
   return (
-    <>
-    
-    <ButtonGroup className="mb-2">
-        {radios.map((radio, idx) => (
-          <ToggleButton
-            key={idx}
-            id={`radio-${idx}`}
-            type="radio"
-            variant="secondary"
-            name="radio"
-            value={radio.value}
-            checked={radioValue === radio.value}
-            style={radioValue === radio.value ? {'backgroundColor':'blue'} : null}
-            onChange={(e) => setRadioValue(e.currentTarget.value)}
-          >
-            {radio.name}
-          </ToggleButton>
-        ))}
-      </ButtonGroup>
-      <div className='signupContainer'>
-      {radioValue === '1' ? <SignupClientForm setUser={setUser} /> : <SignupProForm setUser={setUser}/>}
-      </div>
-    </>
+      <div className="login login-v1">
+				<div className="login-container">
+					<div className="login-header">
+						<div className="brand">
+							<div className="d-flex align-items-center">
+								READY<b>Set</b>BID 
+							</div>
+							<small>Log in or Sign up to Access Features</small>
+						</div>
+						<div className="icon">
+							<i className="fa fa-lock"></i>
+						</div>
+					</div>
+          <div className='signupContainer'>
+            {radioValue === '1' ? <SignupClientForm setUser={setUser} /> : <SignupProForm setUser={setUser}/>}
+          </div>
+          <ButtonGroup className="mb-2">
+            {radios.map((radio, idx) => (
+              <ToggleButton
+                key={idx}
+                id={`radio-${idx}`}
+                type="radio"
+                variant="secondary"
+                name="radio"
+                value={radio.value}
+                checked={radioValue === radio.value}
+                style={radioValue === radio.value ? {'backgroundColor':'blue'} : null}
+                onChange={(e) => setRadioValue(e.currentTarget.value)}
+              >
+                {radio.name}
+              </ToggleButton>
+            ))}
+          </ButtonGroup>
+        </div> 
+		  </div>
   )
 }
