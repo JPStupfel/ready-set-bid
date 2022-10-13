beedd637c1235d633f28e7bb92641ab9dcb9f8c8
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-10-13 13:58:46 -0500

    home has fader

--- client/src/components/LoggedOutHome.js
+++ client/src/components/LoggedOutHome.js
@@ -29,30 +29,30 @@ export default function LoggedOutHome() {
         <div className="coming-soon">
         <div className= "coming-soon-header" id = {!fadeImage ? "coming-soon-header-gif" : "coming-soon-header"}>
 
-            <div className="bg-cover" ></div>
-            <div className="brand">
-                READY<b>Set</b>BID 
+            <div className="bg-cover" >
             </div>
-            <div className="desc">
-            <small>Log in or Sign up to Access Features</small>
-            </div>
-            <div className="timer">
-                <div id="timer" className="is-countdown">
-                  
+            <fader className={fadeProp ? 'fade-in' : 'fade-out'}>  
+                <div className="brand">
+                    READY<b>Set</b>BID 
                 </div>
-            </div>
+                <div className="desc">
+                 <small>Log in or Sign up to Access Features</small>
+                </div>
+            </fader>
         </div>
         <div className="coming-soon-content">
             <div className="desc">
-            Ready-Set-Bid is a bidding wars web application. Let's say you are a homeowner with a huge brush pile in your back yard that you need to have cleaned up. The only problem is, you have no idea how to find someone you can trust to come do the work for a fair price. Enter Ready-Set-Bid. Ready-Set-Bid allows 'Professionals' the opportunity to bid on your project. As a client you simply create an account, upload a few pictures and watch the bids roll in. Once you see a bid that you feel is at a fair price, you click 'accept bid'. That's it.
+            <h1>Ready-Set-Bid is a bidding wars web application. </h1>
+            <br/>Let's say you are a homeowner with a huge brush pile in your back yard that you need to have cleaned up. The only problem is, you have no idea how to find someone you can trust to come do the work for a fair price.
+            <br/><br/>Enter Ready-Set-Bid. Ready-Set-Bid allows 'Professionals' the opportunity to bid on your project. 
+            <br/><br/>As aclient you simply create an account, upload a few pictures and watch the bids roll in. Once you see a bid that you feel is at a fair price, you click 'accept bid'. That's it.
+            <br/><br/>Likewise for professionals, you can:
 
-Likewise for professionals, you can:
+            <br/>1. Create an account.
+            <br/>2. View all the projects which are available for bidding and their location on a map.
+            <br/>3. Make a bid for any number of projects you like.
 
-1. Create an account.
-2. View all the projects which are available for bidding and their location on a map.
-3. Make a bid for any number of projects you like.
-
-Once you have won a bid, that bid is updated to include you as the winner and bidding will be closed.            </div>
+            <br/><br/>Once you have won a bid, that bid is updated to include you as the winner and bidding will be closed.            </div>
            
           
         </div>
