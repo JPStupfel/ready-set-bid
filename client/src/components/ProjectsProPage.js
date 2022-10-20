8cda7fe0419d8ade02eb271162867aeba9029b31
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-10-20 13:25:26 -0500

    pro project page now has side scroll bar

--- client/src/components/ProjectsProPage.js
+++ client/src/components/ProjectsProPage.js
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import MapContainer from './MapContainer'
 import ProjectProfessionalCard from './ProjectProfessionalCard';
 
 export default function ProjectsProPage({projectList}) {
+
+
+  const [thisHeight, setThisHeight] = useState(document.body.clientHeight)
+  useEffect(()=>{setThisHeight(document.body.clientHeight); window.addEventListener('resize', ()=>setThisHeight(document.body.clientHeight));
+  return () => window.removeEventListener('resize', ()=>setThisHeight(document.body.clientHeight));})
+  const scrollStyle={height: `${thisHeight}px`}
+
 
 const projectCards = projectList.map(e=><ProjectProfessionalCard key={e.id} project={e} />)
 
 
   return (
     <div id="gallery" className="gallery row gx-0">
-    <MapContainer projectList={projectList}/>
-          {projectCards}
-    </div>
+            <table>
+              <tbody>
+                <tr>
+                <td id="page_map_container" style={{"width":"60%", "height":"90%"}}>
+                <MapContainer projectList={projectList}/>
+                </td>
+                  <td id="tdSide" width="40%">
+                    <table>
+                      <tbody>
+                        <tr>
+                          <td className='column'>
+                            <div id="results_scroll" class="padding5" style={scrollStyle}>
+                              <div id="roadtripRegionsContainer" class="margin-bottom-45 none" style={{"display": "block;"}}>
+                                <ul id="roadtripRegions" class=" grid-row grid-row--gutter grid-row--col-2 grid-row--card-min-200">
+                                {projectCards}
+                                </ul>
+                              </div>
+                            </div>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
   )
 }
