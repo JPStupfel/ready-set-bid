e01cc09ca4a7be5c7de4f3fb0a33e1309b70fe01
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-10-20 11:55:42 -0500

    changed letter spacing

--- client/src/components/ProjectClientCard.js
+++ client/src/components/ProjectClientCard.js
@@ -17,7 +17,7 @@ function ProjectClientCard({project}) {
                 <img src={image} alt="" />
 							</div>
 							  <div className="image-info">
-								  <h5 className="title" style={{"font-family":"Arial, sans-seri"}}>{project.title}
+								  <h5 className="title" style={{"letter-spacing":"normal"}}>{project.title}
                   </h5>
 								  <div className="d-flex align-items-center mb-2">
 									  <div className="ms-auto">
