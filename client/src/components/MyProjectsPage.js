4696c4f503a69da318ddf5a0e8a3447d357d51ae
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-10-21 07:55:10 -0500

    changing pages fetches new set of 6 projects

--- client/src/components/MyProjectsPage.js
+++ client/src/components/MyProjectsPage.js
@@ -9,18 +9,21 @@ export default function MyProjectsPage() {
   const [projectList, setProjectList] = useState([])
   useEffect(()=>{
     fetch(`proposals?limit=${6}&offset=${offset}`).then(r=>r.json()).then(d=>setProjectList(d)).catch(e=>console.log(e))
-  },[])
-
+  },[offset])
+  // function to change offset +/- int
+  function handleChangeOffset(int){
+    if (offset + int >=0){setOffset(prev=>setOffset(prev+int))}
+    else {console.log('You have reached page 1!')}
+  }
+  // for color coordinating icons
   const openProjectList = projectList.length ? projectList.filter(project=>!project.victor_id) : null
   const closedProjectList = projectList.length ? projectList.filter(projects=>projects.victor_id) : null
+  // for resizing map and scroll bar
   const [thisHeight, setThisHeight] = useState(document.body.clientHeight)
-  
   useEffect(()=>{setThisHeight(document.body.clientHeight); window.addEventListener('resize', ()=>setThisHeight(document.body.clientHeight));
   return () => window.removeEventListener('resize', ()=>setThisHeight(document.body.clientHeight));})
   const scrollStyle={height: `${thisHeight}px`}
-
-// if (!projectList.length){return(<>No Projects Loaded!</>)}
-
+  // create project cards
   const ProjectCards = projectList.map(e=><ProjectClientCard key={e.id} project={e} />)
 
   return (
@@ -42,9 +45,9 @@ export default function MyProjectsPage() {
                                   {ProjectCards}
                                   <span aria-hidden="true" className="" />	
                                   <div className="btn-group">
-                                    <button className="btn btn-outline-inverse">Previous</button>
+                                    <button className="btn btn-outline-inverse" onClick={()=>handleChangeOffset(-6)}>Previous</button>
                                     <button className="btn btn-outline-inverse inactive">Page 1 of 6</button>
-                                    <button className="btn btn-outline-inverse">Next</button>
+                                    <button className="btn btn-outline-inverse" onClick={()=>handleChangeOffset(6)}>Next</button>
                                   </div>
                                 </ul>
                               </div>
