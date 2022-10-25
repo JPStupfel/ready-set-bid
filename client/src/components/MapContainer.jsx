02642d5c5aa86faf8ed0faf7557e3b4721a93ae2
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-10-25 11:41:36 -0500

    mobile map is now inline

--- client/src/components/MapContainer.jsx
+++ client/src/components/MapContainer.jsx
@@ -9,12 +9,16 @@ const API_KEY = process.env.REACT_APP_API_KEY_GOOGLE_EARTH_JS;
   const MapContainer = ({projectList}) => {
 
   const [thisHeight, setThisHeight] = useState(document.body.clientHeight)
+  const [thisWidth, setThisWidth] = useState(document.body.clientWidth)
 
   useEffect(()=>{setThisHeight(document.body.clientHeight); window.addEventListener('resize', ()=>setThisHeight(document.body.clientHeight));
-  return () => window.removeEventListener('resize', ()=>setThisHeight(document.body.clientHeight));})
+  return () => window.removeEventListener('resize', ()=>setThisHeight(document.body.clientHeight));},[])
+  
+  useEffect(()=>{setThisHeight(document.body.clientWidth); window.addEventListener('resize', ()=>setThisWidth(document.body.clientWidth));
+  return () => window.removeEventListener('resize', ()=>setThisWidth(document.body.clientWidth));},[])
 
   const markers = projectList.map(project=><CustomMarker key={project.id} project={project} />)
-  const mapStyles = {height: thisHeight, width: "100%"};
+  const mapStyles = thisWidth > 400 ? {height: thisHeight, width: "100%"} : {height: 300, width: "100%"}
   
   const defaultCenter = {
     lat: projectList.length ? projectList[0].lat : 0,
